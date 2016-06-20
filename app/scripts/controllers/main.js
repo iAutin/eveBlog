1bf6dc1a9e7dedb84bf09ba1989c3fb51d5b8caf
iautin <iautin@compunet.biz>
2016-06-20 10:18:19 -0600

    inital commit 2

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eveBlogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eveBlogApp
+ */
+angular.module('eveBlogApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
